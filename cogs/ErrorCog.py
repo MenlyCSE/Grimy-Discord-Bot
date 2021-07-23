108ed8c1fb8fcf72b9add7cb8ece98f635b14c3c
Inculpable <46857524+Inculpable@users.noreply.github.com>
2021-07-22 20:17:09 -0400

    Update ErrorCog.py

--- cogs/ErrorCog.py
+++ cogs/ErrorCog.py
@@ -39,7 +39,7 @@ class ErrorCog(commands.Cog):
           await ctx.send(embed=embed4, delete_after=5)
           await ctx.message.delete()
       elif isinstance(error, commands.CommandOnCooldown):
-          await ctx.send(f"{ctx.message.author.mention} **Your on cooldown! Try again in {:.2f}s**".format(error.retry_after))
+          await ctx.send(f'{ctx.message.author.mention}', "**Your on cooldown! Try again in {:.2f}s**".format(error.retry_after))
           await ctx.message.delete()
       else:
         raise error
