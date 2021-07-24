627e147ce6943645116ee2d1108c70b27e347454
Inculpable <46857524+Inculpable@users.noreply.github.com>
2021-07-23 22:05:20 -0400

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -257,7 +257,7 @@ bot.load_extension('cogs.MiscCog')
 bot.load_extension('cogs.ErrorCog')
 bot.load_extension('cogs.AdminCogs')
 bot.load_extension('cogs.SearchCog')
-bot.load_extension('cogs.music')
+bot.load_extension('cogs.Music')
 
 bot.run(os.getenv('BOT_TOKEN'))
 
