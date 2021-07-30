0520dfe0191e6c0da15a827508f02cc16443c929
Inculpable <46857524+Inculpable@users.noreply.github.com>
2021-07-30 09:32:29 -0400

    Update HelpCogs.py

--- cogs/HelpCogs.py
+++ cogs/HelpCogs.py
@@ -150,10 +150,10 @@ embed6.add_field(name = "**Sculpting**", value = "**1.** http://y2u.be/L3XtAFUWN
 embed65.add_field(name = "$sculpting", value = "**$modeling**\n**$animating**\n**$graphics** ``GFX``\n**$visuals** ``VFX``")
 
 embed66.add_field(name = "Sculpting", value = "**1.** http://y2u.be/L3XtAFUWNuk\n**2.** http://y2u.be/0lj643VmTsg" )
-embed67.add_field(name = "Modeling", value = "*1.** http://y2u.be/elUJCEC06r8\n**2.** http://y2u.be/6mT4XFJYq-4\n**3.** http://y2u.be/JMBMHSca_j0" )
+embed67.add_field(name = "Modeling", value = "**1.** http://y2u.be/elUJCEC06r8\n**2.** http://y2u.be/6mT4XFJYq-4\n**3.** http://y2u.be/JMBMHSca_j0" )
 embed68.add_field(name = "Animating", value = "**1.** http://y2u.be/uDqjIdI4bF4\n**2.** http://y2u.be/_C2ClFO3FAY" )
-embed69.add_field(name = "Graphics ``GFX``", value = "**1.** http://y2u.be/0F_w4ynybks\n**2.** http://y2u.be/D2WuSJdQKjA" )
-embed70.add_field(name = "Visual FX ``VFX``", value = "***1.** http://y2u.be/-6nBuVHGGuI\n**2.** http://y2u.be/wCge7Y-CmHk" )
+embed69.add_field(name = "Graphics", value = "**1.** http://y2u.be/0F_w4ynybks\n**2.** http://y2u.be/D2WuSJdQKjA" )
+embed70.add_field(name = "Visual FX", value = "**1.** http://y2u.be/-6nBuVHGGuI\n**2.** http://y2u.be/wCge7Y-CmHk" )
 
 embed5.add_field(name = "**FAQ Blender**", value = "**1.** How to install blender?\n**2.** My mesh is see through!\n**3.** How do I subdivide?\n**4.** Blender to Roblox?\n**5.** How to extrude?\n**6.** Change blender layout?\n**7.** Blender experimental?\n**8.** Render faster?")
 
