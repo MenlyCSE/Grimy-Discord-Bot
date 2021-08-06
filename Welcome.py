8a9920b53a323b03eda3e5138e92c20add0a62e1
Inculpable <46857524+Inculpable@users.noreply.github.com>
2021-08-06 08:37:04 -0400

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -287,6 +287,115 @@ async def server(ctx):
     embed116 = discord.Embed().set_thumbnail(url ='https://cdn.discordapp.com/icons/774813762733604865/a_06195d2df5258c5c55752761cb7191fc.gif?size=128&quot').add_field(name="Server's created date:", value="```Sat, 01 May 2020, 02:00 PM EST```").add_field(name="Creator: Inculpable", value="```©️ Reserved rights, article 23 section 15.```")
     await ctx.send(embed=embed116)
         
+async def update_totals(member):
+    invites = await member.guild.invites()
+
+    c = datetime.today().strftime("%Y-%m-%d").split("-")
+    c_y = int(c[0])
+    c_m = int(c[1])
+    c_d = int(c[2])
+
+    async with bot.db.execute("SELECT id, uses FROM invites WHERE guild_id = ?", (member.guild.id,)) as cursor: # this gets the old invite counts
+        async for invite_id, old_uses in cursor:
+            for invite in invites:
+                if invite.id == invite_id and invite.uses - old_uses > 0: # the count has been updated, invite is the invite that member joined by
+                    if not (c_y == member.created_at.year and c_m == member.created_at.month and c_d - member.created_at.day < 7): # year can only be less or equal, month can only be less or equal, then check days
+                        print(invite.id)
+                        await bot.db.execute("UPDATE invites SET uses = uses + 1 WHERE guild_id = ? AND id = ?", (invite.guild.id, invite.id))
+                        await bot.db.execute("INSERT OR IGNORE INTO joined (guild_id, inviter_id, joiner_id) VALUES (?,?,?)", (invite.guild.id, invite.inviter.id, member.id))
+                        await bot.db.execute("UPDATE totals SET normal = normal + 1 WHERE guild_id = ? AND inviter_id = ?", (invite.guild.id, invite.inviter.id))
+
+                    else:
+                        await bot.db.execute("UPDATE totals SET normal = normal + 1, fake = fake + 1 WHERE guild_id = ? and inviter_id = ?", (invite.guild.id, invite.inviter.id))
+
+                    return
+    
+# events
+@bot.event
+async def on_member_join(member):
+    await update_totals(member)
+    await bot.db.commit()
+        
+@bot.event
+async def on_member_remove(member):
+    cur = await bot.db.execute("SELECT inviter_id FROM joined WHERE guild_id = ? and joiner_id = ?", (member.guild.id, member.id))
+    res = await cur.fetchone()
+    if res is None:
+        return
+    
+    inviter = res[0]
+    
+    await bot.db.execute("DELETE FROM joined WHERE guild_id = ? AND joiner_id = ?", (member.guild.id, member.id))
+    await bot.db.execute("DELETE FROM totals WHERE guild_id = ? AND inviter_id = ?", (member.guild.id, memebr.id))
+    await bot.db.execute("UPDATE totals SET left = left + 1 WHERE guild_id = ? AND inviter_id = ?", (member.guild.id, inviter))
+    await bot.db.commit()
+
+@bot.event
+async def on_invite_create(invite):
+    await bot.db.execute("INSERT OR IGNORE INTO totals (guild_id, inviter_id, normal, left, fake) VALUES (?,?,?,?,?)", (invite.guild.id, invite.inviter.id, invite.uses, 0, 0))
+    await bot.db.execute("INSERT OR IGNORE INTO invites (guild_id, id, uses) VALUES (?,?,?)", (invite.guild.id, invite.id, invite.uses))
+    await bot.db.commit()
+    
+@bot.event
+async def on_invite_delete(invite):
+    await bot.db.execute("DELETE FROM invites WHERE guild_id = ? AND id = ?", (invite.guild.id, invite.id))
+    await bot.db.commit()
+
+@bot.event
+async def on_guild_join(guild): # add new invites to monitor
+    for invite in await guild.invites():
+        await bot.db.execute("INSERT OR IGNORE INTO invites (guild_id, id, uses), VAlUES (?,?,?)", (guild.id, invite.id, invite.uses))
+        
+    await bot.db.commit()
+    
+@bot.event
+async def on_guild_remove(guild): # remove all instances of the given guild_id
+    await bot.db.execute("DELETE FROM totals WHERE guild_id = ?", (guild.id,))
+    await bot.db.execute("DELETE FROM invites WHERE guild_id = ?", (guild.id,))
+    await bot.db.execute("DELETE FROM joined WHERE guild_id = ?", (guild.id,))
+
+    await bot.db.commit()
+    
+# commands
+@bot.command()
+async def invites(ctx, member: discord.Member=None):
+    if member is None: member = ctx.author
+
+    # get counts
+    cur = await bot.db.execute("SELECT normal, left, fake FROM totals WHERE guild_id = ? AND inviter_id = ?", (ctx.guild.id, member.id))
+    res = await cur.fetchone()
+    if res is None:
+        normal, left, fake = 0, 0, 0
+
+    else:
+        normal, left, fake = res
+
+    total = normal - (left + fake)
+    
+    em = discord.Embed(
+        title=f"{member.name}#{member.discriminator}",
+        description=f"**{total}** invites. (**{normal}** normal, **{left}** left, **{fake}** fake).",
+        timestamp=datetime.now(),
+        colour=discord.Colour.blurple())
+
+    await ctx.send(embed=em)
+    
+async def setup():
+    await bot.wait_until_ready()
+    bot.db = await aiosqlite.connect("inviteData.db")
+    await bot.db.execute("CREATE TABLE IF NOT EXISTS totals (guild_id int, inviter_id int, normal int, left int, fake int, PRIMARY KEY (guild_id, inviter_id))")
+    await bot.db.execute("CREATE TABLE IF NOT EXISTS invites (guild_id int, id string, uses int, PRIMARY KEY (guild_id, id))")
+    await bot.db.execute("CREATE TABLE IF NOT EXISTS joined (guild_id int, inviter_id int, joiner_id int, PRIMARY KEY (guild_id, inviter_id, joiner_id))")
+    
+    # fill invites if not there
+    for guild in bot.guilds:
+        for invite in await guild.invites(): # invites before bot was added won't be recorded, invitemanager/tracker don't do this
+            await bot.db.execute("INSERT OR IGNORE INTO invites (guild_id, id, uses) VALUES (?,?,?)", (invite.guild.id, invite.id, invite.uses))
+            await bot.db.execute("INSERT OR IGNORE INTO totals (guild_id, inviter_id, normal, left, fake) VALUES (?,?,?,?,?)", (guild.id, invite.inviter.id, 0, 0, 0))
+                                 
+    await bot.db.commit()
+
+
 bot.load_extension('cogs.ToggleCog')
 bot.load_extension('cogs.HelpCogs')
 bot.load_extension('cogs.MiscCog')
@@ -294,7 +403,9 @@ bot.load_extension('cogs.ErrorCog')
 bot.load_extension('cogs.AdminCogs')
 bot.load_extension('cogs.SearchCog')
 
+bot.loop.create_task(setup())
 bot.run(os.getenv('BOT_TOKEN'))
+asyncio.run(bot.db.close())
 
 #member.avatar_url
 #ctx.guild.member
